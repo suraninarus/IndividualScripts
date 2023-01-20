d7bedf926061a78a9db93dcaba2bd1a1e472c288
suraninarus <suraninarus@gmail.com>
2023-01-20 20:21:57 +0100

    The function "fast_multiselect" wa sadded.

--- dijnetszamlaLetolt.py
+++ dijnetszamlaLetolt.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
 
 # path = r"C:\Users\Zsolt\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\names.txt"
 chrome_driver = r"C:\Users\Zsolt\Desktop\learningMaterial\Python\Selenium\drivers\chromedriver_win32\chromedriver.exe"
@@ -31,16 +33,32 @@ username_field.send_keys(Keys.ENTER)
 search_bill_button = driver.find_element(By.XPATH, '//*[@id="logged_menu"]/li[3]/a')
 search_bill_button.click()
 
-# get all elements from the dropdown menus
-drop_down_list = driver.find_elements(By.ID, "sopts")
-# here I should call the Class, that downloads all the bills
-list_element = driver.find_element(By.PARTIAL_LINK_TEXT, str(drop_down_list[1].text))
+# accept cookies
+cookies = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection")
+cookies.click()
 
-for i in drop_down_list:
-    print(i.text)
+# get all elements from the dropdown menus AND select by visible text
+# select = Select(driver.find_elements(By.NAME, "szlaszolgnev"))
+select_text = driver.find_elements(By.NAME, "szlaszolgnev")
+dropdown_options_list = select_text[0].text.split('\n')
 
-def fucker():
-    pass
+for choice_name in dropdown_options_list:
+    select_option = driver.find_element(By.LINK_TEXT, j)
+    print(select_option.text)
+
+# DEF for the funcition
+def fast_multiselect(driver, element_id, labels):
+    select = Select(driver.find_element_by_id(element_id))
+    for label in labels:
+        select.select_by_visible_text(label)
+
+
+# select_option = driver.find_element(By.LINK_TEXT, dropdown_options_list[1])
+select_option = driver.find_element(By.PARTIAL_LINK_TEXT, dropdown_options_list[1])
+print(select_option.text)
+
+# select.select_by_visible_text('Banana')
+
 
 # """         ERROR:
 # selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"partial link text","selector":" BKM NONPROFIT Zrt.
